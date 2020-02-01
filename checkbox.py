0fc89fe1de02c5c2e420cfaa3953b17043a3ade9
jmurray <jonte.murray@gmail.com>
2020-02-01 14:46:09 +0100

    Improve checkbox graphics

--- checkbox.py
+++ checkbox.py
@@ -1,23 +1,25 @@
 from typing import Tuple, Callable, Any, Optional
 
+import pygame
 from pygame.color import Color
 from pygame.math import Vector2
+from pygame.rect import Rect
 
-from ui import Component, FormattedText
+from ui import Component, Text
 from ui import Style
 
 COLOR_WHITE = Color(255, 255, 255)
 
 
 class Checkbox(Component):
-  def __init__(self, size: Tuple[int, int], screen, label: FormattedText, checked: bool = False, **kwargs):
+  def __init__(self, size: Tuple[int, int], screen, label: Text, checked: bool = False, **kwargs):
     super().__init__(size, screen, **kwargs)
     self._callback: Callable[[bool], Any] = kwargs.get('callback')
     self._label = label
     self._style_on_click = kwargs.get('style_onclick')
     self._cooldown = 0
     self._checked = checked
-    self._update_text()
+    self._box = None
 
   def update(self, elapsed_time: int):
     if self._cooldown > 0:
@@ -33,27 +35,31 @@ class Checkbox(Component):
     text_pos = Vector2(self._rect.centerx - self._label.size[0] / 2,
                        self._rect.centery - self._label.size[1] / 2)
     self._label.set_pos(text_pos)
+    w = self._label._rect.h * 0.75
+    self._box = Rect(self._label._rect.right + 10, self._label._rect.bottom - w - 1, w, w)
 
   def _render(self):
     self._label.render()
+    pygame.draw.rect(self._screen, Color(100, 100, 100), self._box)
+    pygame.draw.rect(self._screen, COLOR_WHITE, self._box, 1)
+    if self._checked:
+      pygame.draw.line(self._screen, COLOR_WHITE, self._box.topleft, (self._box.right - 1, self._box.bottom - 1))
+      pygame.draw.line(self._screen, COLOR_WHITE, (self._box.left, self._box.bottom - 1),
+                       (self._box.right - 1, self._box.top))
 
   def _on_click(self, mouse_pos: Optional[Tuple[int, int]]):
     self._checked = not self._checked
-    self._update_text()
     if self._callback:
       self._callback(self._checked)
     self._active_style = self._style_on_click
     self._cooldown = 150
-
-  def _update_text(self):
-    self._label.format_text("x" if self._checked else "_")
 
 
 def checkbox(font, screen, size: Tuple[int, int], callback: Callable[[bool], Any], label: str, checked: bool = False):
   return Checkbox(size=size,
                   screen=screen,
                   callback=callback,
-                  label=FormattedText(screen, font, COLOR_WHITE, label + "  [%s]", None),
+                  label=Text(screen, font, COLOR_WHITE, label),
                   checked=checked,
                   style=Style(background=Color(50, 50, 100), border_color=Color(150, 150, 150)),
                   style_hovered=Style(background=Color(80, 80, 120), border_color=Color(180, 180, 180)),
